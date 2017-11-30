740a2a03de5aeddfadf3f854a90a67614bce92cf
AlirezaMadad <34061432+AlirezaMadad@users.noreply.github.com>
2017-11-30 10:52:03 +0330

    lightweightBackendWebApiServer
    this is a light weight backend server that I designed myself

---
+++ KarYab(MVC)/KarYab/Controllers/AdressApiController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KarYab.Models;
+using System.Data.Entity;
+namespace KarYab.Controllers
+{
+    public class AdressApiController : ApiController
+    {
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public long SetAdress(ADRESS Adress, long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    db.ADRESSES.Add(Adress);
+                    db.SaveChanges();
+                    OWNERSADRES owenersAdress = new OWNERSADRES();
+                    owenersAdress.ADRESSID = Adress.ID;
+                    owenersAdress.ADRESSOWNERID = HumanID;
+                    db.OWNERSADRESES.Add(owenersAdress);
+                    db.SaveChanges();
+                    return Adress.ID;
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public ADRESS GetAdress(long ID, long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    if (db.OWNERSADRESES.Any(r => r.ADRESSOWNERID.Equals(HumanID) && r.ADRESSID.Equals(ID)))
+                    {
+                        return db.ADRESSES.Where(r => r.ID.Equals(ID)).Single();
+                    }
+                    else
+                    {
+                        return new ADRESS();
+                    }
+
+                }
+                catch
+                {
+                    return new ADRESS();
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public bool UpdateAdress(ADRESS Entity)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    // var Skill = db.SKILLS.FirstOrDefault(q => q.ID == SkillID);
+                    db.ADRESSES.Attach(Entity);
+                    var Entry = db.Entry(Entity);
+                    Entry.State = EntityState.Modified;
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public bool DeleteAdress(ADRESS Entity)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    ADRESS entity = db.ADRESSES.FirstOrDefault(q => q.ID == Entity.ID);
+                    OWNERSADRES Owner = db.OWNERSADRESES.FirstOrDefault(q => q.ADRESSID == Entity.ID);
+                    db.OWNERSADRESES.Attach(Owner);
+                    db.OWNERSADRESES.Remove(Owner);
+                    db.SaveChanges();
+                    db.ADRESSES.Attach(entity);
+                    db.ADRESSES.Remove(entity);
+                    db.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public List<ADRESS> GetHumanAdresses(long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    List<long> AdressIds = new List<long>();
+                    AdressIds = db.OWNERSADRESES.Where(r => r.ADRESSOWNERID.Equals(HumanID)).Select(r => r.ADRESSID).ToList();
+                    List<ADRESS> adresses = new List<ADRESS>();
+                    foreach (long item in AdressIds)
+                    {
+                        var adress = db.ADRESSES.Where(r => r.ID.Equals(item)).SingleOrDefault();
+                        adresses.Add(adress);
+                    }
+                    return adresses;
+                }
+                catch (Exception e)
+                {
+                    return new List<ADRESS>();
+                }
+            }
+        }
+    }
+}
---
+++ KarYab(MVC)/KarYab/Controllers/SkillApiController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using KarYab.Models;
+using System.Data.Entity;
+namespace KarYab.Controllers
+{
+    public class SkillApiController : ApiController
+    {
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public long SetSkill(SKILL Skill, long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    db.SKILLS.Add(Skill);
+                    db.SaveChanges();
+                    OWNERSSKILL owenersSkill = new OWNERSSKILL();
+                    owenersSkill.SKILLID = Skill.ID;
+                    owenersSkill.SKILLOWNERID = HumanID;
+                    db.OWNERSSKILLS.Add(owenersSkill);
+                    db.SaveChanges();
+                    return Skill.ID;
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public SKILL GetSkill(long ID, long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    if (db.OWNERSSKILLS.Any(r => r.SKILLOWNERID.Equals(HumanID) && r.SKILLID.Equals(ID)))
+                    {
+                        return db.SKILLS.Where(r => r.ID.Equals(ID)).Single();
+                    }
+                    else
+                    {
+                        return new SKILL();
+                    }
+
+                }
+                catch
+                {
+                    return new SKILL();
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public bool UpdateSkill(SKILL Entity)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                   // var Skill = db.SKILLS.FirstOrDefault(q => q.ID == SkillID);
+                    db.SKILLS.Attach(Entity);
+                    var Entry = db.Entry(Entity);
+                    Entry.State = EntityState.Modified;
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public bool DeleteSkill(SKILL Entity)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    //var employer = new Employ { Id = 1 };
+                    //ctx.Employ.Attach(employer);
+                    //ctx.Employ.Remove(employer);
+                    //ctx.SaveChanges();
+                    SKILL entity = db.SKILLS.FirstOrDefault(q => q.ID == Entity.ID);
+                    OWNERSSKILL Owner = db.OWNERSSKILLS.FirstOrDefault(q => q.SKILLID == Entity.ID);
+                    db.OWNERSSKILLS.Attach(Owner);
+                    db.OWNERSSKILLS.Remove(Owner);
+                    db.SaveChanges();
+                    db.SKILLS.Attach(entity);
+                    db.SKILLS.Remove(entity);
+                    db.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public List<SKILL> GetHumanSkills(long HumanID)
+        {
+            using (KARYABDBEntities db = new KARYABDBEntities())
+            {
+                try
+                {
+                    db.Configuration.ProxyCreationEnabled = false;
+                    List<long> skillIds = new List<long>();
+                    skillIds = db.OWNERSSKILLS.Where(r => r.SKILLOWNERID.Equals(HumanID)).Select(r => r.SKILLID).ToList();
+                    List<SKILL> skills = new List<SKILL>();
+                    foreach (long item in skillIds)
+                    {
+                        skills.Add(db.SKILLS.Where(r => r.ID.Equals(item)).Single());
+                    }
+                    return skills;
+                }
+                catch
+                {
+                    return new List<SKILL>();
+                }
+            }
+        }
+        
+    }
+}
